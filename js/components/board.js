b3837d54b3c7e536bc41694c604217a7da6b3210
annejones817 <annejones817@gmail.com>
2017-01-16 13:08:33 -0800

    React Trello Exercise for Thinkful

---
+++ js/components/board.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var CardList = require('./list')
+
+var Board = function(){
+	return (
+		<div className="board">
+			<CardList />
+			<CardList />
+			<CardList />
+		</div>
+	);
+};
+
+module.exports = Board; 
